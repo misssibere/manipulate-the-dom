0f58f58a84554b982c31285b091c8af60e37ffb7
misssibere <133782108+misssibere@users.noreply.github.com>
2023-06-29 03:15:58 +0000

    HW week 5 day 3

--- script.js
+++ script.js
@@ -13,36 +13,111 @@ document.addEventListener("DOMContentLoaded", function(event) {
     document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
   };
   randomQuote();
-  
+
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
 
+  const changeTitle = document.querySelector("#main-title");
+  changeTitle.textContent = "The DOM HW";
+
 
   // Part 2
+  const changeBackgroundColor = function() {
+    document.body.style.backgroundColor = "gray";
+  };
 
+  changeBackgroundColor();
 
   // Part 3
 
+  function removeListItem() {
+    const favorite_things = document.getElementById("favorite-things");
+    const lastList = favorite_things.lastElementChild;
+    lastList.remove();
+  };
 
+  removeListItem();
   // Part 4
 
 
+  function changeFont_size() {
+    const special_title = document.querySelectorAll(".special-title");
+    special_title.forEach(title => {
+      title.style.fontSize = "2rem";
+    });
+  }
+  changeFont_size();
   // Part 5
 
+  function removeChicago() {
+    const pastRacesList = document.getElementById("past-races");
+    const chicago = pastRacesList.querySelector("li:nth-child(4)");
+    chicago.remove();
+  }
 
+  removeChicago();
   // Part 6
 
 
+
+  function addPastRacesList() {
+    const pastRacesList = document.getElementById("past-races");
+    const newCity = document.createElement("li");
+    newCity.textContent = "Addis Ababa";
+    pastRacesList.appendChild(newCity);
+  }
+  addPastRacesList();
   // Part 7
 
 
+  function createNewPlogPost() {
+    const blogPost_dom_adventures = document.getElementById("dom-adventures");
+      blogPost_dom_adventures.classList.add("special-title")
+  
+
+    const newBlogPostDiv = document.createElement("div");
+    newBlogPostDiv.classList.add("blog-post");
+    newBlogPostDiv.classList.add("purple");
+    const newBlogPosth1 = document.createElement("h1");
+    newBlogPosth1.textContent = `Addis`;
+    const newBlogPostP = document.createElement("p");
+    newBlogPostP.textContent = `What a city to visit.`;
+
+
+    blogPost_dom_adventures.appendChild(newBlogPostDiv, blogPost_dom_adventures.lastChild);
+
+    newBlogPostDiv.appendChild(newBlogPosth1);
+    newBlogPostDiv.appendChild(newBlogPostP);
+
+
+  }
+
+  createNewPlogPost();
+
   // Part 8
 
+  const randomDOMQuote = function() {
+
+
+  };
+  randomDOMQuote();
 
   // Part 9
 
+  function twoEventHandlers() {
+    const blogPosts = document.querySelectorAll(".blog-post .purple");
 
+    blogPosts.forEach(blogPost => {
+      blogPost.addEventListener("mouseout", () => {
+        blogPost.style.backgroundColor = "";
+      });
 
+      blogPost.addEventListener("mouseenter", () => {
+        blogPost.style.backgroundColor = "red";
+      });
+    })
+  };
+  twoEventHandlers();
 
 });
